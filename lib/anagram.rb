ce2f15725a433982be0ea00afafa31132c0de0d6
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-08-21 14:36:56 +0300

    commit tree 1f2cda584c4aefa989757b29cd1707236f7fbdb6

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+    def match(array_names)
+        match_item = []
+        array_names.each do |item|
+            if @word.chars.sort == item.chars.sort
+                match_item << item
+            end
+        end
+        match_item
+    end
+end 
